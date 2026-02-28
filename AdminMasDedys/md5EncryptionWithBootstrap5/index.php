8a861286e1fc1e273b661f3091bd617f62c90e38
masdedys <kakashi.setiawan@gmail.com>
2026-02-28 10:37:02 +0700

    add data

--- AdminMasDedys/md5EncryptionWithBootstrap5/index.php
+++ AdminMasDedys/md5EncryptionWithBootstrap5/index.php
@@ -4,54 +4,82 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="refresh" content="30">
-    <title>md-5 encryption by mas_dedys</title>
-    <link rel="shortcut icon" href="img/logo.png" />
+    <title>md5 encryption | masdedys.github.io</title>
+    <link rel="shortcut icon" href="../../assets/img/logo.png" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <!-- Bootstrap icon -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-</head>
+    <style>
+        .copyright {
+            background-color: #fff;
+        }
+        .copyright-item small {
+            display: flex;
+            height: 60px;
+            padding: 0;
+            margin: 0;
+            justify-content: center;
+            align-items: center;
+            color: #000;
+        }
+        .copyright-item small a {
+            text-decoration: none;
+            color: #000;
+        }
+        .copyright-item small a:hover {
+            color: #434343;
+        }
+        .copyright small{
+            font-size: 16px;
+        }
+        /* Hidding sections under 960px width */
+        @media screen and (max-width: 600px) {
+            .copyright small{
+                font-size: 14px;
+            }
+        }
+    </style>
+  </head>
   <body>
-    <section class="container content position-absolute top-50 start-50 translate-middle">
-        <div class="row text-center">
-            <div class="col-md">
-                <h2>MD5 Encryption</h2>
-            </div>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-md-6">
-                <form action="" method="post">
-                        <label for="text" class="mb-3">Tulis text yang akan disandikan:</label><br>
-                        <input type="text" class="form-control mb-3" id="text" placeholder="Masukkan Text" name="text_asli">
-                        <button type="submit" name="send" class="btn btn-primary mb-3"><i class="bi bi-key-fill"></i> Sandikan</button>
+    <div class="container">
+      <div class="row d-flex justify-content-center align-items-center" style="height: 85vh;">
+        <div class="col-md-6 text-center">
+          <h2>MD5 Encryption</h2>
+          <form action="" method="post">
+            <label for="text" class="mb-3">Tulis text yang akan disandikan:</label><br>
+            <input type="text" class="form-control mb-3" id="text" placeholder="Masukkan Text" name="text_asli">
+            <button type="submit" name="send" class="btn btn-primary mb-3"><i class="bi bi-key-fill"></i> Sandikan</button>
                     
-                        <?php
-                            if (isset($_POST['send'])) {
-                            $text_asli = $_POST['text_asli'];
-                            $text_eknripsi = md5($_POST['text_asli']);
-                            echo "
-                            <div class=\"mb-3\">
-                            <label class=\"form-label\">Text asli anda:</label>
-                                <input type=\"text\" class=\"form-control\" value=\"$text_asli\" disabled>
-                            </div>
-                            <div class=\"mb-3\">
-                            <label class=\"form-label\">Tersandi sebagai:</label>
-                                <input type=\"text\" class=\"form-control\" value=\"$text_eknripsi\" disabled>
-                            </div>";
-                            }
-                        ?>
-                </form>
-            </div>
-        </div>
-    </section>
-    <section class="credit">
-      <div class="container-fluid fixed-bottom p-4" style="background-color: #e2edff;">
-        <div class="row">
-          <div class="col-md">
-            <p class="text-center">Created with <i class="bi bi-cup-hot-fill"></i> and <i class="bi bi-heart-fill"></i> <a href="https://masdedys.github.io/"> by mas_dedys</a>. | &copy; 2023</p>
-          </div>
+              <?php
+                if (isset($_POST['send'])) {
+                  $text_asli = $_POST['text_asli'];
+                  $text_eknripsi = md5($_POST['text_asli']);
+                  echo "
+                    <div class=\"mb-3\">
+                      <label class=\"form-label\">Text asli anda:</label>
+                      <input type=\"text\" class=\"form-control\" value=\"$text_asli\" disabled>
+                    </div>
+                    <div class=\"mb-3\">
+                      <label class=\"form-label\">Tersandi sebagai:</label>
+                      <input type=\"text\" class=\"form-control\" value=\"$text_eknripsi\" disabled>
+                    </div>";
+                  }
+                ?>
+          </form>
         </div>
       </div>
-    </section>
+    </div>
+    <!-- copyright start -->
+      <div class="copyright container-fluid">
+            <div class="copyright-item container">
+                <div class="row">
+                    <div class="col-md text-dark">
+                        <small>&copy;2026&nbsp;<a href="https://masdedys.github.io/">masdedys.github.io</a>, All Rights Reserved</small>
+                    </div>
+                </div>
+            </div>
+      </div>
+    <!-- copyright end -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
   </body>
 </html>
--- AdminMasDedys/numberToTextWithPHP/index.php
+++ AdminMasDedys/numberToTextWithPHP/index.php
@@ -3,48 +3,74 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Ubah Angka menjadi Huruf | by mas_dedys</title>
-    <link rel="shortcut icon" href="img/logo.png" />
+    <title>Change number into text (Indonesia) | masdedys.github.io</title>
+    <link rel="shortcut icon" href="../../assets/img/logo.png" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <!-- Bootstrap icon -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-</head>
-  <body>
-    <section class="content container position-absolute top-50 start-50 translate-middle">
-        <div class="row text-center">
-            <div class="col-md">
-                <h2>Ubah Angka menjadi Huruf</h2>
-            </div>
-        </div>
-        <div class="row text-center">
-            <div class="col-md">
-                <form action="" method="get">
-                    <label for="text" class="mb-3">Masukan angka:</label><br>
-                    <input type="number" class="form-control mb-3" id="text" placeholder="Masukkan Angka" name="angka">
-                    <button type="submit" name="send" class="btn btn-primary mb-3"><i class="bi bi-justify-left"></i> Proses</button>
-                </form>
-            </div>
-        </div>
-        <?php
-        if (isset($_GET['send'])) {
-            include "cetakhurufdariangka.php";
-            $angka = $_GET['angka'];
-            $formatangka = number_format($angka, 0, ",", ",");
-            $tertulisangka = ucwords(tertulis($angka));
-            echo "<label for=\"text\" class=\"mb-3\"><b>Angka Anda: $formatangka</b></label><br>";
-            echo "<label for=\"text\" class=\"mb-3\"><b>Tertulis: $tertulisangka</b></label><br>";
+    <style>
+        .copyright {
+            background-color: #fff;
         }
-        ?>
-    </section>
-    <section class="credit">
-      <div class="container-fluid fixed-bottom p-4" style="background-color: #e2edff;">
-        <div class="row">
-          <div class="col-md">
-            <p class="text-center">Created with <i class="bi bi-cup-hot-fill"></i> and <i class="bi bi-heart-fill"></i> <a href="https://masdedys.github.io/"> by mas_dedys</a>. | &copy; 2023</p>
-          </div>
+        .copyright-item small {
+            display: flex;
+            height: 60px;
+            padding: 0;
+            margin: 0;
+            justify-content: center;
+            align-items: center;
+            color: #000;
+        }
+        .copyright-item small a {
+            text-decoration: none;
+            color: #000;
+        }
+        .copyright-item small a:hover {
+            color: #434343;
+        }
+        .copyright small{
+            font-size: 16px;
+        }
+        /* Hidding sections under 960px width */
+        @media screen and (max-width: 600px) {
+            .copyright small{
+                font-size: 14px;
+            }
+        }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <div class="row d-flex justify-content-center align-items-center" style="height: 85vh;">
+        <div class="col-md-6">
+          <h2>Ubah Angka menjadi Huruf</h2>
+          <form action="" method="get">
+            <label for="text" class="mb-3">Masukan angka:</label><br>
+            <input type="number" class="form-control mb-3" id="text" placeholder="Masukkan Angka" name="angka">
+            <button type="submit" name="send" class="btn btn-primary mb-3">Proses</button>
+          </form>
+          <?php
+            if (isset($_GET['send'])) {
+                include "cetakhurufdariangka.php";
+                $angka = $_GET['angka'];
+                $formatangka = number_format($angka, 0, ",", ",");
+                $tertulisangka = ucwords(tertulis($angka));
+                echo "<label for=\"text\" class=\"mb-3\"><b>Angka: $formatangka</b></label><br>";
+                echo "<label for=\"text\" class=\"mb-3\"><b>Terbilang: $tertulisangka</b></label><br>";
+            }
+          ?>
         </div>
       </div>
-    </section>
+    </div>
+    <!-- copyright start -->
+      <div class="copyright container-fluid">
+            <div class="copyright-item container">
+                <div class="row">
+                    <div class="col-md text-dark">
+                        <small>&copy;2026&nbsp;<a href="https://masdedys.github.io/">masdedys.github.io</a>, All Rights Reserved</small>
+                    </div>
+                </div>
+            </div>
+      </div>
+    <!-- copyright end -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
     <script src="script.js"></script>
